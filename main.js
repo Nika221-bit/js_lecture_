d35977210b779a493bff90da73ff22072caa794d
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-08 17:05:59 +0400

    1

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let neo = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+for (let i = 0; i < neo.length; i++) {
+    console.log(neo[i])
+  for(let j = 0;j < neo[i].length;j++){
+    console.log(neo[i][j])
+  }
+}
